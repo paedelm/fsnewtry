614454b5234ef954fbaceb733e8f64dea6c75235
Peter Edelman <peter.edelman@nn.nl>
2019-06-28 16:56:56 +0200

    working with fsi and dotnet run together

--- Program.fs
+++ Program.fs
@@ -2,10 +2,13 @@
 
 open System
 open Ftest
+open proc
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
     do makeFileTransfers
+    let outp, _ = proc.runProc "cmd" "/c hoi.cmd" None
+    for line in outp do printfn "%s" line
     0 // return an integer exit code
 

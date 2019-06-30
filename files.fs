c75955c8ba38a1e47cf5c737ae434a0a014c7306
Peter <paedelm@gmail.com>
2019-06-30 22:44:57 +0200

    corrections

--- files.fs
+++ files.fs
@@ -9,12 +9,12 @@ module files
             seq { yield! dirs |> Seq.collect Directory.EnumerateFiles
                   yield! dirs |> Seq.collect Directory.EnumerateDirectories |> allFiles }
 
-    let rec allFilesInfo info (dirs:seq<string>) =
-        if Seq.isEmpty dirs then Seq.empty else
-            seq { 
-                (for d in dirs do (yield! DirectoryInfo(d).EnumerateFiles) |> Seq.collect info
-                yield dirs |> Seq.collect Directory.EnumerateDirectories |> allFiles
-                }
+    // let rec allFilesInfo info (dirs:seq<string>) =
+    //     if Seq.isEmpty dirs then Seq.empty else
+    //         seq { 
+    //             (for d in dirs do (yield! DirectoryInfo(d).EnumerateFiles) |> Seq.collect info
+    //             yield dirs |> Seq.collect Directory.EnumerateDirectories |> allFiles
+    //             }
 
-open files
-let peter = allFiles [@"c:\users\p_ede\projects"] |> Seq.filter (fun (n:string) -> n.EndsWith(".fs"))
+// open files
+// let peter = allFiles [@"c:\users\p_ede\projects"] |> Seq.filter (fun (n:string) -> n.EndsWith(".fs"))
